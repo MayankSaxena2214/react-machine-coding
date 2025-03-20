83ce6589676e071fb9c060c3bd34102b08014438
Mayank Saxena <mayanksaxena2214@gmail.com>
2025-03-20 11:53:03 +0530

    Multiple toast comppleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Accordian from './components/Accordian'
 import ToastContainer from './components/ToastContainer'
-import "./App.css"
+import "./App.css";
+/*
+In normal toast we just showed the success toast single
+but now we will show multiple toasst like success, warning, etc
+Also suppose multiple toast at the same time also. Like error and warning at the same time
+so we need to maining the list of toast
+
+ */
+import AdvancedToast from './components/AdvancedToast'
 const App = () => {
   return (
     <div className='bg-blue-50'>
       {/* <Accordian/> */}
-      <ToastContainer/>
+      {/* <ToastContainer/> */}
+      <AdvancedToast/>
     </div>
   )
 }
---
+++ src/components/AdvancedToast.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const AdvancedToast = () => {
+    const [show,setShow]=useState(null);
+    const [toastsList,setToastList]=useState([]);
+    const colorsMapping={
+        success:"green",
+        warning:"yellow",
+        error:"red",
+        info:"blue"
+    }
+    const handleClose=(id)=>{
+        setToastList((prevToasts)=>{
+            let newToastList=prevToasts.filter((item)=>item.id!=id);
+            return newToastList;
+        })
+    }
+    const handleClick=(message,type)=>{
+        const id=new Date().getTime();
+        console.log(id);
+        setToastList([...toastsList,{id,message,type}]);
+        
+        setTimeout(()=>handleClose(id),5000);
+    }
+
+  return (
+    <div className='py-10 px-10 flex flex-col gap-10'>
+      <div className='fixed top-1 right-1 flex gap-2 flex-col'>
+       {
+        toastsList.map((item)=>{
+            return  <div key={item.id} className='flex justify-between w-[300px] rounded-xl toast py-4 px-4' style={{
+                backgroundColor:colorsMapping[item.type]
+            }}>
+            <div>{item?.message}</div>
+            <div className='cursor-pointer' onClick={()=>handleClose(item.id)}>X</div>
+        </div>
+        })
+       }
+      </div>
+      <div className='flex gap-4'>
+        <button onClick={()=>handleClick("Success","success")} className='px-6 py-3 bg-red-400 text-white'>Success Toast</button>
+        <button onClick={()=>handleClick("Warning","warning")} className='px-6 py-3 bg-red-400 text-white'>Warning Toast</button>
+        <button onClick={()=>handleClick("Error","error")} className='px-6 py-3 bg-red-400 text-white'>Error Toast</button>
+        <button onClick={()=>handleClick("Info","info")} className='px-6 py-3 bg-red-400 text-white'>Info Toast</button>
+      </div>
+    </div>
+  )
+}
+
+export default AdvancedToast
